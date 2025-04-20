008c3e226ad45d676ce0d36ded4ea688c2dfa40f
Yoo-Ha-Young <bluekitten74@naver.com>
2025-04-21 01:28:32 +0900

    init : initalize project

---
+++ src/test/java/com/project/Notering/NoteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Notering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
